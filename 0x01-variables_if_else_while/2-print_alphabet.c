e118d3c0324b6ea868730126091e80e544b0f3aa
zineddine <zinoutebib24@gmail.com>
2023-12-16 00:57:29 +0100

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i < 123)
+	{
+	    putchar(i);
+	    i++;
+	}
+	printf("\n");
+
+	return (0);
+}
