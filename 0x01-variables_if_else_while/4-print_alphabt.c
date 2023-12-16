f4e1ac9bf0ab3ff8df170caeb8921ceafe463b25
zineddine <zinoutebib24@gmail.com>
2023-12-16 01:58:08 +0100

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet letters exept e and q
+ *
+ * Return: Alaways (0)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter < 'z')
+	{
+		if ((letter != 'e') && (letter != 'q'))
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
