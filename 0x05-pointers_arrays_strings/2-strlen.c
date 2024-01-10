2359688212e57194ab68a77801aa3e4d66c8c23b
zineddine <zinoutebib24@gmail.com>
2024-01-10 06:07:34 +0100

    add : 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - get the length of the string
+ * @s: the string
+ * Return: the length of the sting (integer)
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
