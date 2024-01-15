8e805523e2c8980e62330569a170073c4383ea11
zineddine <zinoutebib24@gmail.com>
2024-01-15 08:36:04 +0100

    change the order of condition loop to 1-stracat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,7 +19,7 @@ char *_strncat(char *dest, char *src, int n)
 		len_dest++;
 	}
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[len_dest] = src[i];
 		len_dest++;
