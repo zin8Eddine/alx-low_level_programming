9222d48a9663bef68e05cbafec037e588969bb4e
zineddine <zinoutebib24@gmail.com>
2024-01-18 19:23:28 +0100

    add 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strncpy - Write a function that copies a string, with number of bytes
+* @dest: This is the output dest
+* @src: This is the input source
+* @n: This is the number of bytes to copy
+*
+*Return: Copied string with the number of bytes
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[n] = '\0';
+	return (dest);
+}
