416c85fcafabbf5719320bdd22ed7ce4bcde877c
zineddine <zinoutebib24@gmail.com>
2024-01-23 11:21:06 +0100

    Update 4-rev_array.c file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - prints reverse array.
+ * @a: array to be compared.
+ * @n: size of array.
+ * Return: reversed array.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		tmp = a[i];
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = tmp;
+	}
+}
